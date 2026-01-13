65b1becd1142ab28b20716428dfb6afd38a1e3f7
kalpanavaka <kalpanavakaid@gmail.com>
2026-01-13 16:51:43 +1100

    Update playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from '@playwright/test';
 export default defineConfig({
   testDir: './tests',
   use: {
-    headless: false,
+    headless: true,
     acceptDownloads: true,
     viewport: { width: 1280, height: 720 },
   },
