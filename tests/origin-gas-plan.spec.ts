318cf1ac42464583b6ea67bcf1277d8b0a012c79
kalpanavaka <kalpanavakaid@gmail.com>
2026-01-13 18:02:31 +1100

    Update origin-gas-plan.spec.ts

--- tests/origin-gas-plan.spec.ts
+++ tests/origin-gas-plan.spec.ts
@@ -5,18 +5,18 @@ import { PlanDetailsPage } from '../pages/PlanDetailsPage';
 import { extractPdfText } from '../utils/pdfUtils';
 
 test('Origin Energy – Gas plan PDF validation', async ({ page, context }) => {
+  // 1. Navigate to the pricing page and enter the test address
   const pricingPage = new PricingPage(page);
   await pricingPage.goto();
-
   await pricingPage.searchAddress('17 Bolinda Road, Balwyn North, VIC 3104');
 
   const plansPage = new PlansPage(page);
   await plansPage.verifyPlansDisplayed();
-   //await page.pause(); -> to clearly see the plans are displayed
+    // 2. Filtered the results to show Gas plans only
   await plansPage.uncheckElectricity();
   await plansPage.verifyPlansDisplayed();
-  //await page.pause();  -> to clearly see the plans are displayed after unchecking the electricity
 
+  // 3. Click the first plan and wait for it to open in a new tab
   const [planPage] = await Promise.all([
     context.waitForEvent('page'),
     plansPage.clickPlanLink(),
@@ -24,10 +24,11 @@ test('Origin Energy – Gas plan PDF validation', async ({ page, context }) => {
 
   await planPage.waitForLoadState('domcontentloaded');
 
-  //await page.pause();-> to clearly see that it has opened in new tab
+  // 4. Download the PDF from the new tab and save it to the test-results folder
   const planDetails = new PlanDetailsPage(planPage);
   const pdfPath = await planDetails.downloadPlanPdf('test-results');
 
+  // 5. Read the PDF content and verify it contains the word "gas"
   const pdfText = await extractPdfText(pdfPath);
   expect(pdfText.toLowerCase()).toContain('gas');
 });
